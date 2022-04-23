cfa7a8b9933c0ac9fd97a01349fa8b9b1eebbb90
DESKTOP-3KOTKPG\cesar <cesar.cazs@gmail.com>
2022-04-23 19:52:20 +0200

    Adding stylings, some coments to update in a readme and dockerfile preparing

--- src/components/commons/ButtonSubmit.tsx
+++ src/components/commons/ButtonSubmit.tsx
@@ -4,10 +4,7 @@ type TypeProps = { isLogged: boolean; dispatcher: any; password: string };
 export default function ButtonSubmit({ isLogged, dispatcher, password }: TypeProps) {
   const dispatch = useDispatch();
   return (
-    <button
-      type="submit"
-      onClick={() => dispatch(dispatcher(!isLogged))}
-      disabled={!password ? true : false}>
+    <button type="submit" onClick={() => dispatch(dispatcher(!isLogged))}>
       {isLogged ? 'Logout' : 'Login'}
     </button>
   );
