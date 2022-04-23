789b8c7669eb675af6595b78a805f3aba3d8b270
DESKTOP-3KOTKPG\cesar <cesar.cazs@gmail.com>
2022-04-23 18:53:28 +0200

    Prevent broke app with checkbox

--- src/components/commons/InputCheckbox.tsx
+++ src/components/commons/InputCheckbox.tsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TypeTodo } from '../../models/TypeTodoList';
 
 type componentCheckbox = { dispatcher: any; todo: TypeTodo };
 export default function InputCheckbox({ dispatcher, todo }: componentCheckbox) {
   const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
 
-  function onChangeAction(event: any) {
-    console.log(event);
+  function onChangeAction() {
+    setChecked(!checked);
   }
 
   return (
     <>
       <p>{todo.checked.toString()}</p>
-      <input type="checkbox" onClick={() => dispatch(dispatcher(todo))} checked={todo.checked} />
+      <input
+        type="checkbox"
+        onClick={() => dispatch(dispatcher(todo))}
+        onChange={onChangeAction}
+        checked={todo.checked}
+      />
     </>
   );
 }
