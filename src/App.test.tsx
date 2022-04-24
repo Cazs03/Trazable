2e0b799da37d4e8593f18c1f8a6e102f2e418e62
DESKTOP-3KOTKPG\cesar <cesar.cazs@gmail.com>
2022-04-24 19:48:54 +0200

    Adding testing coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,14 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
+import InputCheckbox from './components/commons/InputCheckbox';
 import InputPassword from './components/commons/InputPassword';
+import LabelCombined from './components/commons/LabelCombined';
 import Login from './components/features/Login';
 import Main from './components/views/Main';
 import UserStore from './redux/store/UserStore';
+import { checkTodoListInLocalStorage, getLastIdFromArray } from './utils/getLastIdFromArray';
+import { setLocalStorage } from './utils/localStorage';
 
 test('Render App', () => {
   render(<App />);
@@ -38,4 +42,57 @@ test('Login InputPassword', async () => {
   expect(usernameInput).toHaveValue(fakePass);
   fireEvent.change(usernameInput, { target: { value: '000' } });
   expect(fakeCb).toHaveBeenCalled();
+});
+
+test('Check/unchecked InputCheckbox', async () => {
+  const mockTodo = {
+    id: 0,
+    description: 'bailar',
+    checked: true,
+  };
+  render(
+    <Provider store={UserStore}>
+      <InputCheckbox dispatcher={null} todo={mockTodo} />
+    </Provider>
+  );
+  const inputCheckbox = screen.queryByPlaceholderText(/bailar/i);
+});
+
+test('setLocalStorage', () => {
+  const mockTodo = {
+    id: 0,
+    description: 'bailar',
+    checked: true,
+  };
+  expect(setLocalStorage([mockTodo]));
+});
+
+test('getLastIdFromArray', () => {
+  const mockTodo = {
+    id: 0,
+    description: 'bailar',
+    checked: true,
+  };
+  const lastId = getLastIdFromArray([mockTodo]);
+  expect(getLastIdFromArray([mockTodo]));
+  expect(lastId).toBe(0);
+});
+
+test('checkTodoListInLocalStorage', () => {
+  const lastId = checkTodoListInLocalStorage(0);
+  console.log(lastId);
+  expect(lastId).toBe(undefined);
+});
+
+test('LabelCombined', async () => {
+  // const mockTodo = {
+  //   id: 0,
+  //   description: 'bailar',
+  //   checked: true,
+  // };
+  render(
+    <Provider store={UserStore}>
+      <LabelCombined description={'awesome'} IncludeComponent={null} />
+    </Provider>
+  );
 });
